c15b37dc4653f1f5ea0effacf0b2f44b1525425b
TheTopLuca <ali.hany@student.guc.edu.eg>
2018-10-09 01:06:10 +0200

    Add files via upload

--- src/SaveWesteros/SaveWesteros.java
+++ src/SaveWesteros/SaveWesteros.java
@@ -28,8 +28,8 @@ public class SaveWesteros extends SearchProblem {
 		o.add("attack");
 		o.add("getDragonGlass");
 		o.add("up");
-		o.add("down");
 		o.add("left");
+		o.add("down");
 		o.add("right");
 		this.setOperators(o);
 		setStateSpace(new ArrayList<>());
@@ -51,8 +51,8 @@ public class SaveWesteros extends SearchProblem {
 	public void genGrid() {
 		
 		Random r = new Random();
-		int randomX = r.nextInt(1) + 3;//7,4
-		int randomY = r.nextInt(1) + 3;//7,4
+		int randomX = r.nextInt(1) + 6;//7,4
+		int randomY = r.nextInt(1) + 6;//7,4
 		maxDragonGlass = r.nextInt(10) + 1;
 		this.positionI = randomX - 1;
 		this.positionJ = randomY - 1;
@@ -130,6 +130,7 @@ public class SaveWesteros extends SearchProblem {
 	
 	public Node Search(Cell [][] grid, String strategy, boolean visualize) {
 		Node n = new Node(0, 0, null, null, null, this.getInitialState());
+		getStateSpace().add(this.getInitialState());
 		ArrayList<Node> nodes = new ArrayList<>();
 		nodes.add(n);
 		while(!nodes.isEmpty()) {
@@ -138,6 +139,8 @@ public class SaveWesteros extends SearchProblem {
 			}else {
 				Node current = nodes.remove(0);
 				if(goalTest(current.getState())) {
+					System.out.println(nodes.toString());
+					System.out.println(nodes.size());
 					return current;//return node
 				}else {
 					switch(strategy) {
@@ -151,6 +154,7 @@ public class SaveWesteros extends SearchProblem {
 				}
 			}
 		}
+		System.out.println("No Solution");
 		return null;
 	}
 	
@@ -158,12 +162,14 @@ public class SaveWesteros extends SearchProblem {
 		//this.setStateSpace(transition(current));
 		ArrayList<StateWithOperator> possibleStates = transition(current);
 		for (int i = 0; i < possibleStates.size(); i++) {
-			Node n = new Node(current.getDepth()+1, current.getCost()+1, current, null, possibleStates.get(i).getOperator(), possibleStates.get(0).getState());
-			nodes.add(n);
-			System.out.println(n.getState().getState().toString());
+			if(!((possibleStates.get(i)).getState()).checkSameState(getStateSpace())){
+				System.out.println("Check Complete");
+				Node n = new Node(current.getDepth()+1, current.getCost()+1, current, null, possibleStates.get(i).getOperator(), possibleStates.get(i).getState());
+				nodes.add(n);
+				getStateSpace().add(possibleStates.get(i).getState());
+				System.out.println(nodes.toString());
+			}
 		}
-		System.out.println(nodes.toString());
-		System.out.println(nodes.size());
 		return nodes;
 	}
 	
@@ -198,8 +204,9 @@ public class SaveWesteros extends SearchProblem {
 		String whiteWalkersPositions = currentState.getState().get(4);
 		String [] parsedWhite = (whiteWalkersPositions.split("/"));
 		for (int k = 0; k < parsedWhite.length; k++) {
-			int posX = Character.getNumericValue(parsedWhite[k].charAt(0));
-			int posY = Character.getNumericValue(parsedWhite[k].charAt(2));
+			String [] splitComma = parsedWhite[k].split(",");
+			int posX = Integer.parseInt(splitComma[0]);
+			int posY = Integer.parseInt(splitComma[1]);
 			if(posX == i && posY == j) {
 				return true;
 			}
@@ -224,12 +231,10 @@ public class SaveWesteros extends SearchProblem {
 			case "up" :
 				if(positionI != 0 &&!parseStateWhite(currentState, positionI-1, positionJ)) {
 				State newState = generateState(positionI-1, positionJ, numWhiteWalkers, currentDragonGlass, whiteWalkersPositions);
-				System.out.println("up");
 				s.add(new StateWithOperator(newState, "up"));
 			}break;
 			case "down" :
 				if(positionI != grid.length-1 && !parseStateWhite(currentState, positionI+1, positionJ)) {
-				System.out.println("down");
 				State newState = generateState(positionI+1, positionJ, numWhiteWalkers, currentDragonGlass, whiteWalkersPositions);
 				s.add(new StateWithOperator(newState, "down"));
 			}break;
@@ -237,29 +242,30 @@ public class SaveWesteros extends SearchProblem {
 				if(positionJ != 0 && !parseStateWhite(currentState, positionI, positionJ-1)) {
 				State newState = generateState(positionI, positionJ-1, numWhiteWalkers, currentDragonGlass, whiteWalkersPositions);
 				s.add(new StateWithOperator(newState, "left"));
-				System.out.println("left");
 			}break;
 			case "right" :
 				if(positionJ != grid[0].length-1 && !parseStateWhite(currentState, positionI, positionJ+1)) {
 				State newState = generateState(positionI, positionJ+1, numWhiteWalkers, currentDragonGlass, whiteWalkersPositions);
 				s.add(new StateWithOperator(newState, "right"));
-				System.out.println("right");
 			}break;
 			case "getDragonGlass" :
-				if(grid[positionI][positionJ].getType().equals("dragonStone") && currentDragonGlass != maxDragonGlass) {
+				if((grid[positionI][positionJ].getType()).equals("dragonStone") && currentDragonGlass != maxDragonGlass) {
 				State newState = generateState(positionI, positionJ, numWhiteWalkers, maxDragonGlass, whiteWalkersPositions);
-				s.add(new StateWithOperator(newState, "getDragonGlass"));
-				System.out.println("DS");
+				s.add(new StateWithOperator(newState, "getDragonGlass")); 
 			}break;
 			case "attack" :
 				State newState = checkValidAttack(currentState);
 				if(newState!=null) {
 					s.add(new StateWithOperator(newState, "attack"));
-					System.out.println("Attack");
 				}
 				break;
 			}
 		}
+		System.out.println(positionI+","+positionJ+" "+grid[positionI][positionJ].getType());
+		for (int i = 0; i < s.size(); i++) {
+			System.out.println((s.get(i).getState().getState().toString()));
+		}
+		
 		return s;
 	}
 
@@ -315,7 +321,9 @@ public class SaveWesteros extends SearchProblem {
 		} 
 		String newWhitePositions = "";
 		for (int k = 0; k < parsedWhitePositions.length; k++) {
-			newWhitePositions+=parsedWhitePositions[k]+"/";
+			if(!parsedWhitePositions[k].equals("")){
+				newWhitePositions+=parsedWhitePositions[k]+"/";
+			}
 		}
 		return newWhitePositions;
 	}
@@ -325,8 +333,8 @@ public class SaveWesteros extends SearchProblem {
 		s.genGrid();
 		//State st = s.generateState(0, 0, s.getNumWhiteWalkers(), s.getMaxDragonGlass(), s.getWhiteWalkersPositions());
 		//System.out.println(s.removeWhiteWalker(st, s.getPositionI()-1, s.getPositionJ()));
-		s.printGrid();
 		System.out.println(s.Search(s.getGrid(), "BF", false));
+		s.printGrid();
 	}
 
 	public Cell[][] getGrid() {
--- src/state/State.java
+++ src/state/State.java
@@ -18,4 +18,23 @@ public class State {
 		this.state = state;
 	}
 	
+	public  boolean checkSameState(ArrayList<State> state1){
+		for(int i = 0 ; i<state1.size() ; i++) {
+			 State currentState = state1.get(i);
+			 boolean checkStateElement = true;
+			 for(int k=0 ; k<5 ; k++){
+				 System.out.print("The first state compared : "  + currentState.getState().toString() + ",");
+				 System.out.print("The current state being enqueued : " + this.getState().toString());
+				 System.out.println();
+				 
+				 if(!(currentState.getState().get(k)).equals(this.getState().get(k))){
+					 checkStateElement= false;
+				 }
+			 }
+			 if(checkStateElement == true) return true;
+		}
+		return false;
+		
+	}
+	
 }
